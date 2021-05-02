4884466de77e7e60212e1b9c9d46a28a8e75f50c
Jaime <jaimeferncast@gmail.com>
2021-05-02 12:06:05 +0200

    create EditableMenu component

---
+++ client/src/components/editableMenu/EditableMenu.js
@@ -0,0 +1,58 @@
+import { Component } from 'react'
+import initialData from '../../assets/initial-data'
+import { DragDropContext } from 'react-beautiful-dnd'
+import Category from './Category'
+
+class EditableMenu extends Component {
+  state = initialData
+
+  onDragEnd = result => {
+    const { destination, source, draggableId } = result
+
+    if (!destination) {
+      return
+    }
+
+    if (
+      destination.droppableId === source.droppableId &&
+      destination.index === source.index
+    ) {
+      return
+    }
+
+    const column = this.state.columns[source.droppableId]
+    const newTaskIds = Array.from(column.taskIds)
+    newTaskIds.splice(source.index, 1)
+    newTaskIds.splice(destination.index, 0, draggableId)
+
+    const newColumn = {
+      ...column,
+      taskIds: newTaskIds,
+    }
+
+    const newState = {
+      ...this.state,
+      columns: {
+        ...this.state.columns,
+        [newColumn.id]: newColumn,
+      },
+    }
+
+    this.setState(newState)
+  }
+
+  render() {
+    return (
+      <DragDropContext onDragEnd={this.onDragEnd}>
+        {this.state.columnOrder.map(columnId => {
+          const column = this.state.columns[columnId]
+          const tasks = column.taskIds.map(taskId => this.state.tasks[taskId])
+
+          return <Category key={column.id} column={column} tasks={tasks} />
+        })}
+      </DragDropContext>
+    )
+  }
+}
+
+export default EditableMenu
