a5c825e8a0eb401058ac82da321984ffc98ca1af
Jaime <jaimeferncast@gmail.com>
2021-05-03 16:55:20 +0200

    create basic login page

--- server/routes/users.routes.js
+++ server/routes/users.routes.js
@@ -1,7 +1,7 @@
 const express = require("express")
 const router = express.Router()
 const bcrypt = require("bcrypt")
-// const { checkIfAdmin } = require("../middlewares")
+const { checkRights } = require("../middlewares")
 
 const User = require("../models/user.model")
 
