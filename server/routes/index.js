e8c94d67788e59ea5d25bb2f3d00175cfc021d37
Jaime <jaimeferncast@gmail.com>
2021-05-14 11:14:18 +0200

    create menus routes and update routes index

--- server/routes/index.js
+++ server/routes/index.js
@@ -2,6 +2,7 @@ module.exports = app => {
 
     app.use('/api/auth', require('./auth.routes.js'))
     app.use('/api/users', require('./users.routes.js'))
+    app.use('/api/menus', require('./menus.routes.js'))
     app.use('/api/categories', require('./categories.routes.js'))
     app.use('/api/products', require('./products.routes.js'))
     app.use('/api/orders', require('./orders.routes.js'))
--- server/routes/menus.routes.js
+++ server/routes/menus.routes.js
@@ -1,47 +1,46 @@
 const express = require('express')
 const router = express.Router()
-const Category = require('../models/category.model')
+const Menu = require('../models/menu.model')
 
 router.get('/', (_req, res) =>
-  Category.find()
-    .sort({ index: 1 })
-    .then((categories) => res.json({ message: categories }))
+  Menu.find()
+    .then((menus) => res.json({ message: menus }))
     .catch((error) =>
       res.status(500).json({
         code: 500,
-        message: 'Error buscando las categorías',
+        message: 'Error buscando las cartas',
         error: error.message,
       })
     )
 )
 
 router.post('/new', (req, res) => {
-  const category = { ...req.body }
-  Category.create(category)
+  const menu = { ...req.body }
+  Menu.create(menu)
     .then((response) => res.json(response))
     .catch((err) =>
       res.status(500).json({
         code: 500,
-        message: "No se ha podido crear la categoría",
+        message: "No se ha podido crear la carta",
         error: err.message
       })
     )
 })
 
 router.delete('/:_id', (req, res) => {
-  Category.findByIdAndDelete(req.params._id)
+  Menu.findByIdAndDelete(req.params._id)
     .then((response) => res.json(response))
-    .catch((err) => res.status(500).json({ code: 500, message: "No se ha podido borrar la categoría", error: err.message }))
+    .catch((err) => res.status(500).json({ code: 500, message: "No se ha podido borrar la carta", error: err.message }))
 })
 
 router.put('/:_id', (req, res) => {
-  const categoryData = ({ name, index } = req.body)
-  Category.findByIdAndUpdate(req.params._id, categoryData, { new: true })
+  const menuData = ({ name, index } = req.body)
+  Menu.findByIdAndUpdate(req.params._id, menuData, { new: true })
     .then((response) => res.json(response))
     .catch((err) =>
       res.status(500).json({
         code: 500,
-        message: "No se ha podido modificar la categoría",
+        message: "No se ha podido modificar la carta",
         error: err.message
       })
     )
