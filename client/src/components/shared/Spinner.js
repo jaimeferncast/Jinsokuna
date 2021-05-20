daec682b8fe16ab23560b9b7f5fb60c9872fd230
Jaime <jaimeferncast@gmail.com>
2021-05-20 16:26:18 +0200

    change position and thickness of spinner

--- client/src/components/shared/Spinner.js
+++ client/src/components/shared/Spinner.js
@@ -3,13 +3,13 @@ import styled from "styled-components"
 import { CircularProgress, Grid } from "@material-ui/core"
 
 const CustomCircularProgress = styled(CircularProgress)`
-  margin-top: 50px;
+  margin-top: 200px;
 `
 
 function Spinner(props) {
   return (
-    <Grid container justify="center">
-      <CustomCircularProgress size={80} thickness={3} />
+    <Grid container justify="center" alignItems="center">
+      <CustomCircularProgress size={80} thickness={2.5} />
     </Grid>
 
   )
