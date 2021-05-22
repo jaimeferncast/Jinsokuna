7c73a207f4346fc5637dd89aab26e3b3f2d7cf36
Jaime <jaimeferncast@gmail.com>
2021-05-22 19:54:07 +0200

    change button control of padding

--- client/src/components/layout/Navigation.js
+++ client/src/components/layout/Navigation.js
@@ -9,17 +9,22 @@ import { AppBar, Toolbar, Grid, Button } from "@material-ui/core"
 import { withTheme } from "@material-ui/core/styles"
 import RestaurantMenuIcon from "@material-ui/icons/RestaurantMenu"
 
+import CustomButton from "../shared/CustomButton"
+
 import AuthService from "../../service/auth.service"
 
+const Navbar = styled(AppBar)`
+  position: fixed;
+  background-color: ${props => props.theme.palette.sophisticated.dark};
+`
 const MenuItem = styled.div`
   margin-right: 20px;
-  & .MuiButton-outlined {
-    border: 1px solid rgba(0, 0, 0, 0.50);
-  }
-  & .MuiButton-text {
-    border: 1px solid rgba(0, 0, 0, 0);
+  & .MuiButton-root {
     padding: 5px 15px;
   }
+`
+const Logo = styled(RestaurantMenuIcon)`
+  color: ${props => props.theme.palette.sophisticated.primary};
 `
 
 class Navigation extends Component {
@@ -42,51 +47,44 @@ class Navigation extends Component {
 
   render() {
     return (
-      <AppBar position="fixed">
+      <Navbar>
         <Toolbar>
           <Grid container justify="space-between" alignItems="center" wrap="nowrap">
             <Grid item>
-              <Button
-                disabled
-                startIcon={
-                  <RestaurantMenuIcon
-                    style={{ fontSize: '28px', color: this.props.theme.palette.secondary.main }}
-                  />
-                }
-              ></Button>
+              <Logo fontSize="large" />
             </Grid>
             {this.state.mobile
               ? <DropDownMenu {...this.props} />
               : <Grid item style={{ display: "flex" }}>
                 <MenuItem>
                   <Link to="/">
-                    <Button variant={this.props.location.pathname === "/" ? "outlined" : "text"}>
+                    <CustomButton variant={this.props.location.pathname === "/" && "outlined"}>
                       Resumen de órdenes
-                </Button>
+                </CustomButton>
                   </Link>
                 </MenuItem>
                 <MenuItem>
                   <Link to="/carta">
-                    <Button variant={this.props.location.pathname === "/carta" ? "outlined" : "text"}>
+                    <CustomButton variant={this.props.location.pathname === "/carta" && "outlined"}>
                       confección de carta
-                </Button>
+                </CustomButton>
                   </Link>
                 </MenuItem>
                 <MenuItem>
                   <Link to="/usuario">
-                    <Button variant={this.props.location.pathname === "/usuario" ? "outlined" : "text"}>
+                    <CustomButton variant={this.props.location.pathname === "/usuario" && "outlined"}>
                       Gestión de usuarios
-                </Button>
+                </CustomButton>
                   </Link>
                 </MenuItem>
               </Grid>
             }
             <Grid item>
-              <Button variant="contained" color="secondary" onClick={this.logoutUser}>Cerrar sesión</Button>
+              <CustomButton variant="filled" onClick={this.logoutUser}>Cerrar sesión</CustomButton>
             </Grid>
           </Grid>
         </Toolbar>
-      </AppBar>
+      </Navbar>
     )
   }
 }
