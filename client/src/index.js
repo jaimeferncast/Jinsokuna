adf80b8ca0a31f569b25ae1c4fd26c3f21e6c0e8
Jaime <jaimeferncast@gmail.com>
2021-05-23 19:27:04 +0200

    move custom theme provider to index, wrapping app component

--- client/src/index.js
+++ client/src/index.js
@@ -1,12 +1,16 @@
 import React from "react"
 import ReactDOM from "react-dom"
 import { BrowserRouter as Router } from "react-router-dom"
+
+import { CustomThemeProvider } from "./ThemeContext"
 import App from "./components/App"
 
 ReactDOM.render(
   <React.StrictMode>
     <Router>
-      <App />
+      <CustomThemeProvider>
+        <App />
+      </CustomThemeProvider >
     </Router>
   </React.StrictMode>,
   document.getElementById('root')
