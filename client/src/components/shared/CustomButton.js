a6e6f9e3d3834c466dfabd53b81708fa1cca256d
Jaime <jaimeferncast@gmail.com>
2021-05-23 19:26:32 +0200

    adjust custom button to theme

--- client/src/components/shared/CustomButton.js
+++ client/src/components/shared/CustomButton.js
@@ -1,21 +1,29 @@
+import { useContext } from "react"
+
 import styled from "styled-components"
 
 import { Button } from "@material-ui/core"
 
+import ThemeContext from "../../ThemeContext"
+
 const StyledButton = styled(Button)`
-  color: ${props => props.theme.palette.sophisticated.light};
-  border: 1px solid ${props => props.variant === "outlined"
-    ? props.theme.palette.sophisticated.primary
+  font-family: ${props => props.font};
+  color: ${props => props.palette.light};
+  border: 2px solid ${props => props.type === "outlined"
+    ? props.palette.primary.main
     : "rgba(0,0,0,0)"};
-  background-color: ${props => props.variant === "filled" && props.theme.palette.sophisticated.primary};
+  background-color: ${props => props.type === "filled" && props.palette.primary.main};
   &:hover {
-    background-color: ${props => props.variant === "filled" && props.theme.palette.sophisticated.primaryLight};
+    background-color: ${props => props.type === "filled" && props.palette.primaryLight};
   }
+  padding: 5px 15px;
 `
 
 function CustomButton(props) {
+  const { palette, font } = useContext(ThemeContext)
+
   return (
-    <StyledButton {...props}>
+    <StyledButton palette={palette} font={font} {...props}>
       {props.children}
     </StyledButton>
   )
