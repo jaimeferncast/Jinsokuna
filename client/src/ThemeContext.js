484600812d6c6b43b9c137b017182075f836faab
Jaime <jaimeferncast@gmail.com>
2021-05-23 19:27:34 +0200

    create custom theme context with react context

---
+++ client/src/ThemeContext.js
@@ -0,0 +1,42 @@
+import { Component, createContext } from "react"
+
+import theme from "./components/theme"
+
+const ThemeContext = createContext()
+
+class CustomThemeProvider extends Component {
+
+  state = {
+    palette: "sophisticated",
+    font: "arial",
+  }
+
+  changePalette(palette) {
+    this.setState({ palette })
+  }
+
+  changeFont(font) {
+    this.setState({ font })
+  }
+
+  render() {
+    const { children } = this.props
+    const { palette, font } = this.state
+    const { changePalette, changeFont } = this
+
+    return (
+      <ThemeContext.Provider value={{
+        palette: theme.palette[palette],
+        font: theme.font[font],
+        changePalette,
+        changeFont,
+      }}>
+        {children}
+      </ThemeContext.Provider>
+    )
+  }
+}
+
+export default ThemeContext
+
+export { CustomThemeProvider }
