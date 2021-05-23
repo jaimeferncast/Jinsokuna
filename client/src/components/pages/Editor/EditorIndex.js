d5985971545ee2763fb855dfa09222c3b14c5fbf
Jaime <jaimeferncast@gmail.com>
2021-05-23 19:29:38 +0200

    adjust component to theme context

--- client/src/components/pages/Editor/EditorIndex.js
+++ client/src/components/pages/Editor/EditorIndex.js
@@ -1,5 +1,8 @@
+import { useContext } from "react"
+
 import styled from "styled-components"
 
+import ThemeContext from "../../../ThemeContext"
 import Navigation from "../../layout/Navigation"
 import Orders from "./Orders/Orders"
 import EditMenu from "./EditMenu/EditMenu"
@@ -7,17 +10,19 @@ import EditUser from "./EditUser/EditUser"
 
 const Main = styled.main`
   padding: 80px 30px 70px;
-  background-color: ${props => props.theme.palette.sophisticated.secondary};
+  background-color: ${props => props.palette.secondary};
   min-height: 100vh;
 `
 
 function EditorIndex(props) {
+  const { palette } = useContext(ThemeContext)
+
   return (
     <>
       <Navigation
         storeUser={props.storeUser}
       />
-      <Main>
+      <Main palette={palette}>
         {props.location.pathname === "/carta"
           ? <EditMenu />
           : props.location.pathname === "/usuario"
