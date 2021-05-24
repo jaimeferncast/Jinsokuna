6124eb51d63fb17b767fbaab79b87c9236bee249
Jaime <jaimeferncast@gmail.com>
2021-05-24 17:13:47 +0200

    add background transparency

--- client/src/components/pages/Editor/EditMenu/SubNavigation.js
+++ client/src/components/pages/Editor/EditMenu/SubNavigation.js
@@ -1,22 +1,29 @@
+import { useContext } from "react"
+
 import styled from "styled-components"
 
-import { Button, Grid } from "@material-ui/core"
+import { Grid } from "@material-ui/core"
+
+import ThemeContext from "../../../../ThemeContext"
+import CustomButton from "../../../shared/CustomButton"
 
 const Container = styled(Grid)`
-  height: 56px;
+  height: 80px;
   position: fixed;
   bottom: 0;
   left: 0;
   z-index: 999;
-  background-color: ${props => props.theme.palette.primary.main};
+  background-color: ${props => props.palette.dark + '99'};
 `
-const SubButton = styled(Button)`
+const SubButton = styled(CustomButton)`
   margin: 0 20px;
 `
 
 function SubNavigation(props) {
+  const { palette } = useContext(ThemeContext)
+
   return (
-    <Container container justify="center" alignItems="center">
+    <Container palette={palette} container justify="center" alignItems="center">
       <SubButton
         variant="contained"
         color="primary"
@@ -24,7 +31,10 @@ function SubNavigation(props) {
       >
         guardar cambios
       </SubButton>
-      <SubButton variant="contained" color="primary">
+      <SubButton
+        variant="contained"
+        color="primary"
+      >
         confección de menús
       </SubButton>
     </Container>
