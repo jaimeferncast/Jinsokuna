02a2bccc972fe08cdf3f8be539164fe5a7728ba3
Jaime <jaimeferncast@gmail.com>
2021-05-24 17:10:43 +0200

    theme adjustments

--- client/src/components/shared/CustomButton.js
+++ client/src/components/shared/CustomButton.js
@@ -7,23 +7,17 @@ import { Button } from "@material-ui/core"
 import ThemeContext from "../../ThemeContext"
 
 const StyledButton = styled(Button)`
-  font-family: ${props => props.font};
-  color: ${props => props.palette.light};
-  border: 2px solid ${props => props.type === "outlined"
+  border: 2px solid ${props => props.variant === "outlined"
     ? props.palette.primary.main
     : "rgba(0,0,0,0)"};
-  background-color: ${props => props.type === "filled" && props.palette.primary.main};
-  &:hover {
-    background-color: ${props => props.type === "filled" && props.palette.primaryLight};
-  }
   padding: 5px 15px;
 `
 
 function CustomButton(props) {
-  const { palette, font } = useContext(ThemeContext)
+  const { palette } = useContext(ThemeContext)
 
   return (
-    <StyledButton palette={palette} font={font} {...props}>
+    <StyledButton palette={palette} {...props}>
       {props.children}
     </StyledButton>
   )
