e3960f5a3e551969f0c7d27ee23080768fbb1b02
Jaime <jaimeferncast@gmail.com>
2021-05-26 15:49:30 +0200

    change error handling and add inMenus property to update category

--- server/routes/categories.routes.js
+++ server/routes/categories.routes.js
@@ -6,45 +6,27 @@ router.get('/', (_req, res) =>
   Category.find()
     .sort({ index: 1 })
     .then((categories) => res.json({ message: categories }))
-    .catch((error) =>
-      res.status(500).json({
-        code: 500,
-        message: 'Error buscando las categorías',
-        error: error.message,
-      })
-    )
+    .catch((err) => res.status(500).json({ ...err, message: err.message }))
 )
 
 router.post('/new', (req, res) => {
   const category = { ...req.body }
   Category.create(category)
     .then((response) => res.json(response))
-    .catch((err) =>
-      res.status(500).json({
-        code: 500,
-        message: "No se ha podido crear la categoría",
-        error: err.message
-      })
-    )
+    .catch((err) => res.status(500).json({ ...err, message: err.message }))
 })
 
 router.delete('/:_id', (req, res) => {
   Category.findByIdAndDelete(req.params._id)
     .then((response) => res.json(response))
-    .catch((err) => res.status(500).json({ code: 500, message: "No se ha podido borrar la categoría", error: err.message }))
+    .catch((err) => res.status(500).json({ ...err, message: err.message }))
 })
 
 router.put('/:_id', (req, res) => {
-  const categoryData = ({ name, index } = req.body)
+  const categoryData = ({ name, index, inMenus } = req.body)
   Category.findByIdAndUpdate(req.params._id, categoryData, { new: true })
     .then((response) => res.json(response))
-    .catch((err) =>
-      res.status(500).json({
-        code: 500,
-        message: "No se ha podido modificar la categoría",
-        error: err.message
-      })
-    )
+    .catch((err) => res.status(500).json({ ...err, message: err.message }))
 })
 
 module.exports = router
