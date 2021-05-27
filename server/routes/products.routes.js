f5f2b0c0d87af0d249c2e49287a4cac24fb514f6
Jaime <jaimeferncast@gmail.com>
2021-05-27 20:50:35 +0200

    add delete products of only deleted categories when fetching products

--- server/routes/products.routes.js
+++ server/routes/products.routes.js
@@ -4,9 +4,25 @@ const Product = require('../models/product.model')
 const Category = require('../models/category.model')
 
 router.get('/', async (_req, res) => {
-  Product.find()
-    .then((products) => res.json({ message: products }))
-    .catch((err) => res.status(500).json({ ...err, message: err.message }))
+  try {
+    const categories = await Category.find()
+    const products = await Product.find()
+    const categoryIds = categories.map(cat => cat._id.toString())
+    const productsWithoutCategory = products.filter(prod => {
+      return !prod.categories.some(cat => {
+        return categoryIds.includes(cat.id.toString())
+      })
+    })
+
+    const deletedProducts = await Promise.all(productsWithoutCategory.map(prod => {
+      return Product.findByIdAndDelete(prod._id)
+    }))
+
+    Product.find()
+      .then((products) => res.json({ message: products, deletedProducts }))
+  } catch (err) {
+    res.status(500).json({ ...err, message: err.message })
+  }
 })
 
 router.post('/new', (req, res) => {
