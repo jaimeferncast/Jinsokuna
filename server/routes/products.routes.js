146bd955f0fefd59f4bc6397c26bcfa00084c4de
Jaime <jaimeferncast@gmail.com>
2021-05-27 18:02:05 +0200

    change error response

--- server/routes/products.routes.js
+++ server/routes/products.routes.js
@@ -4,55 +4,29 @@ const Product = require('../models/product.model')
 const Category = require('../models/category.model')
 
 router.get('/', async (_req, res) => {
-  try {
-    const categories = await Category.find().select("_id")
-    const products = await Product.find().select("category")
-    products.map(prod => {
-      if (!categories.some(cat => cat._id.toString() == prod.category.toString()))
-        Product.findByIdAndDelete(prod._id).then()
-    })
-
-    Product.find().then((products) => res.json({ message: products }))
-  }
-  catch (error) {
-    res.status(500).json({
-      code: 500,
-      message: 'Error buscando los productos',
-      error: error.message,
-    })
-  }
+  Product.find()
+    .then((products) => res.json({ message: products }))
+    .catch((err) => res.status(500).json({ ...err, message: err.message }))
 })
 
 router.post('/new', (req, res) => {
   const product = { ...req.body }
   Product.create(product)
     .then((response) => res.json(response))
-    .catch((err) =>
-      res.status(500).json({
-        code: 500,
-        message: "No se ha podido crear el producto",
-        error: err.message
-      })
-    )
+    .catch((err) => res.status(500).json({ ...err, message: err.message }))
 })
 
 router.delete('/:_id', (req, res) => {
   Product.findByIdAndDelete(req.params._id)
     .then((response) => res.json(response))
-    .catch((err) => res.status(500).json({ code: 500, message: "No se ha podido borrar el producto", err }))
+    .catch((err) => res.status(500).json({ ...err, message: err.message }))
 })
 
 router.put('/:_id', (req, res) => {
-  const productData = ({ name, index } = req.body)
+  const productData = ({ name, isMenu, description, categories, price, allergies, menuProduct } = req.body)
   Product.findByIdAndUpdate(req.params._id, productData, { new: true })
     .then((response) => res.json(response))
-    .catch((err) =>
-      res.status(500).json({
-        code: 500,
-        message: "No se ha podido modificar el producto",
-        error: err.message
-      })
-    )
+    .catch((err) => res.status(500).json({ ...err, message: err.message }))
 })
 
 module.exports = router
