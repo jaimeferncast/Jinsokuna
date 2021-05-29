5d8ff266eed1fe2413b5db8d6e86ee99707025bb
Jaime <jaimeferncast@gmail.com>
2021-05-29 14:16:11 +0200

    add use theme service in theme context to fetch and update DB palette and font

--- client/src/ThemeContext.js
+++ client/src/ThemeContext.js
@@ -2,21 +2,35 @@ import { Component, createContext } from "react"
 
 import theme from "./components/theme"
 
+import ThemeService from "./service/theme.service"
+
 const ThemeContext = createContext()
 
 class CustomThemeProvider extends Component {
 
   state = {
-    palette: "sophisticated",
-    font: "verdana",
+    palette: "",
+    font: "",
+  }
+
+  themeService = new ThemeService()
+
+  componentDidMount = async () => {
+    const [palette] = (await this.themeService.getPalettes()).data.message
+    const [font] = (await this.themeService.getFonts()).data.message
+    this.setState({ palette: palette.name, font: font.name })
   }
 
   changePalette = (palette) => {
-    this.setState({ palette })
+    this.themeService.updatePalette(palette)
+      .then(() => this.setState({ palette }))
+      .catch((err) => alert(err))
   }
 
   changeFont = (font) => {
-    this.setState({ font })
+    this.themeService.updateFont(font)
+      .then(() => this.setState({ font }))
+      .catch((err) => alert(err))
   }
 
   render() {
@@ -25,7 +39,9 @@ class CustomThemeProvider extends Component {
     const { changePalette, changeFont } = this
 
     return (
+      (this.state.palette && this.state.font) &&
       <ThemeContext.Provider value={{
+        theme: theme,
         palette: theme.palette[palette],
         font: theme.font[font],
         changePalette,
