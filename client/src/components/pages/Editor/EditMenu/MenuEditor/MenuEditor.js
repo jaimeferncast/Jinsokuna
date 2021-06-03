0f9b40b70107c2313c62a9bd3aab4113e819b4ca
Jaime <jaimeferncast@gmail.com>
2021-06-03 07:39:19 +0200

    advance with menu editor

--- client/src/components/pages/Editor/EditMenu/MenuEditor/MenuEditor.js
+++ client/src/components/pages/Editor/EditMenu/MenuEditor/MenuEditor.js
@@ -1,10 +1,8 @@
 import { Component, PureComponent } from "react"
 
-import styled from "styled-components"
-
 import { DragDropContext, Droppable } from "react-beautiful-dnd"
 
-import { Typography, Grid, TextField, Button, Divider } from "@material-ui/core"
+import { Grid, TextField, Button } from "@material-ui/core"
 import DeleteForeverIcon from "@material-ui/icons/DeleteForever"
 import EditIcon from "@material-ui/icons/Edit"
 
@@ -27,17 +25,17 @@ class InnerList extends PureComponent {
     const {
       category,
       index,
-      products,
       showConfirmationMessage,
       editCategory,
+      removeProduct,
     } = this.props
 
     return <MenuCategory
       category={category}
       index={index}
-      products={products}
       showConfirmationMessage={showConfirmationMessage}
       editCategory={editCategory}
+      removeProduct={removeProduct}
     />
   }
 }
@@ -51,6 +49,7 @@ class MenuEditor extends Component {
     this.state = {
       menu: props.menu,
       showMenuInput: false,
+      otherMenus: undefined,
       isMenuProducts: undefined,
       openModal: false,
       modalProduct: null,
@@ -69,7 +68,8 @@ class MenuEditor extends Component {
     try {
       const products = (await this.menuService.getProducts()).data.message
       const isMenuProducts = products.filter(prod => prod.isMenuProduct)
-      this.setState({ isMenuProducts })
+      const otherMenus = products.filter(prod => prod.isMenu && prod._id !== this.props.menu._id)
+      this.setState({ isMenuProducts, otherMenus })
     }
     catch (error) {
       this.setState({
@@ -105,15 +105,16 @@ class MenuEditor extends Component {
     })
   }
 
-  showConfirmationMessage = (category) => {
-    category
+  showConfirmationMessage = (index, name) => {
+    index
       ? this.setState({
         alert: {
           open: true,
           message: "¿Seguro que quieres borrar la categoría?",
           severity: "warning",
           vertical: "top",
-          category,
+          i: index,
+          id: name,
         }
       })
       : this.setState({
@@ -144,7 +145,31 @@ class MenuEditor extends Component {
 
   MenuInputSubmit = (e) => {
     e ? e.preventDefault() : window.removeEventListener('mousedown', this.handleClick)
-    this.setState({ showMenuInput: false })
+
+    if (this.state.otherMenus.some(elm =>
+      elm.name.toUpperCase() === this.state.menu.name.toUpperCase()
+    )) this.setState({
+      menu: {
+        ...this.state.menu,
+        name: this.props.menu.name
+      },
+      alert: {
+        open: true,
+        severity: "error",
+        message: `Ya existe un menú con el nombre ${this.state.menu.name.toUpperCase()}`,
+        vertical: "bottom",
+      }
+    })
+    else {
+      this.props.editMenuProduct(this.state.menu)
+      this.setState({
+        showMenuInput: false,
+        alert: {
+          ...this.state.alert,
+          open: false,
+        }
+      }, () => this.updateBD(this.state.menu._id, this.state.menu))
+    }
   }
 
   addCategory = (e, category) => {
@@ -173,21 +198,17 @@ class MenuEditor extends Component {
     else {
       const menu = { ...this.state.menu }
       menu.menuContent.push({ categoryName: category })
-      this.menuService.updateProduct(menu._id, menu)
-        .then(res => this.setState({ menu: res.data }))
-        .catch(() => this.setState({
-          alert: {
-            open: true,
-            severity: "error",
-            message: "Error de servidor",
-            vertical: "bottom",
-          }
-        }))
+      this.setState({
+        alert: {
+          ...this.state.alert,
+          open: false,
+        }
+      }, () => this.updateBD(menu._id, menu))
     }
   }
 
   editCategory = (category) => {
-    if (this.state.menu.menuContent.some(cat =>
+    if (this.state.menu.menuContent.filter(elm => elm._id !== category._id).some(cat =>
       cat.categoryName.toUpperCase() === category.categoryName.toUpperCase()
     )) this.setState({
       alert: {
@@ -211,24 +232,39 @@ class MenuEditor extends Component {
       const menu = { ...this.state.menu }
       const categoryIndex = menu.menuContent.findIndex(elm => elm._id === category._id)
       menu.menuContent.splice(categoryIndex, 1, category)
-      this.menuService.updateProduct(menu._id, menu)
-        .then(res => this.setState({ menu: res.data }))
-        .catch(() => this.setState({
-          alert: {
-            open: true,
-            severity: "error",
-            message: "Error de servidor",
-            vertical: "bottom",
-          }
-        }))
+      this.setState({
+        alert: {
+          ...this.state.alert,
+          open: false,
+        }
+      }, () => this.updateBD(menu._id, menu))
     }
   }
 
-  deleteCategory = (id) => {
+  deleteCategory = (id, index) => {
     const menu = { ...this.state.menu }
-    const categoryIndex = menu.menuContent.findIndex(elm => elm._id === id)
-    menu.menuContent.splice(categoryIndex, 1)
-    this.menuService.updateProduct(menu._id, menu)
+    menu.menuContent.splice(index - 1, 1)
+    this.setState({
+      alert: {
+        ...this.state.alert,
+        open: false,
+      }
+    }, () => this.updateBD(menu._id, menu))
+  }
+
+  removeProduct = (productIndex, categoryIndex) => {
+    const menu = { ...this.state.menu }
+    menu.menuContent[categoryIndex - 1].products.splice(productIndex - 1, 1)
+    this.setState({
+      alert: {
+        ...this.state.alert,
+        open: false,
+      }
+    }, () => this.updateBD(menu._id, menu))
+  }
+
+  updateBD = (id, data) => {
+    this.menuService.updateProduct(id, data)
       .then(res => this.setState({ menu: res.data }))
       .catch(() => this.setState({
         alert: {
@@ -238,9 +274,11 @@ class MenuEditor extends Component {
           vertical: "bottom",
         }
       }))
-
   }
 
+  goBack = () => {
+    this.props.deselectMenu()
+  }
 
   render() {
     const { palette } = this.context
@@ -250,10 +288,15 @@ class MenuEditor extends Component {
         <> {/* title fragment */}
           <Grid container justify="flex-start" style={{ margin: "0 auto", width: "1008px" }}>
             {this.state.showMenuInput
-              ? <form onSubmit={this.MenuInputSubmit} style={{ width: "500px", margin: "-17px 80px 0 0" }}>
+              ? <form
+                onSubmit={this.MenuInputSubmit}
+                style={{ width: "500px", margin: "-17px 80px 0 0" }}
+                autoComplete="off"
+              >
                 <Grid container justify="space-between" alignItems="flex-end" style={{ paddingLeft: "50px" }}>
                   <Grid item xs={7}>
                     <TextField
+                      autoComplete={false}
                       fullWidth
                       name="name"
                       label="Nombre de la Carta"
@@ -285,7 +328,7 @@ class MenuEditor extends Component {
                 </Grid>
               </form>
               : <Title variant="h5" noWrap>
-                {capitalizeTheFirstLetterOfEachWord(this.props.menu.name)}
+                {capitalizeTheFirstLetterOfEachWord(this.state.menu.name)}
               </Title>
             }
             {!this.state.showMenuInput &&
@@ -328,15 +371,14 @@ class MenuEditor extends Component {
               >
                 {this.state.menu.menuContent
                   .map((elm, index) => {
-                    return <p>{elm.categoryName}</p>
-                    // <InnerList
-                    //   key={elm._id}
-                    //   category={elm.categoryName}
-                    //   index={index}
-                    //   products={elm.products}
-                    //   showConfirmationMessage={(category) => this.showConfirmationMessage(category)}
-                    //   editCategory={(category) => this.editCategory(category)}
-                    // />
+                    return <InnerList
+                      key={elm._id}
+                      category={elm}
+                      index={index + 1}
+                      showConfirmationMessage={(category, name) => this.showConfirmationMessage(category, name)}
+                      editCategory={(category) => this.editCategory(category)}
+                      removeProduct={(productIndex, categoryIndex) => this.removeProduct(productIndex, categoryIndex)}
+                    />
                   })}
                 {provided.placeholder}
               </Container>
@@ -353,8 +395,8 @@ class MenuEditor extends Component {
           open={this.state.alert.open}
           message={this.state.alert.message}
           severity={this.state.alert.severity}
-          id={this.state.alert.category?._id}
-          category={this.state.alert.category}
+          i={this.state.alert.i}
+          id={this.state.alert.id}
           closeAlert={(message, severity) => this.closeAlert(message, severity)}
           deleteMenu={this.props.deleteMenuProduct}
           deleteCategory={(i, id) => this.deleteCategory(id, i)}
