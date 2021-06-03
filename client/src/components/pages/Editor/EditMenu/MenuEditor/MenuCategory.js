aa22ee8548d8ef58c1ba256ac7b721c5e2ed3a2b
Jaime <jaimeferncast@gmail.com>
2021-06-03 07:39:45 +0200

    finish menucategory

--- client/src/components/pages/Editor/EditMenu/MenuEditor/MenuCategory.js
+++ client/src/components/pages/Editor/EditMenu/MenuEditor/MenuCategory.js
@@ -1,57 +1,16 @@
 import { Component } from "react"
 
-import styled from "styled-components"
-
 import { Droppable, Draggable } from "react-beautiful-dnd"
 
-import { Typography, Button, Grid, TextField, Divider } from "@material-ui/core"
+import { Button, Grid, Divider } from "@material-ui/core"
 import DeleteForeverIcon from "@material-ui/icons/DeleteForever"
 import EditIcon from "@material-ui/icons/Edit"
-import AddBoxIcon from "@material-ui/icons/AddBox"
 
 import ThemeContext from "../../../../../ThemeContext"
 import MenuProduct from "./MenuProduct"
-import CustomButton from "../../../../shared/CustomButton"
+import { CategoryContainer, TitleGrid, Title, TitleInput, ProductList } from "../CarteEditor/Category"
 
 import { capitalizeTheFirstLetterOfEachWord } from "../../../../../utils"
-
-const CategoryContainer = styled.div`
-  margin: 5px 0;
-  padding: 5px 10px;
-  background-color: ${props => props.palette.dark};
-  border-radius: 5px;
-  width: 100%;
-  max-width: 548px;
-  display: flex;
-  flex-direction: column;
-`
-const TitleGrid = styled(Grid)`
-  padding: 0 10px 0 0;
-`
-const Title = styled(Typography)`
-  padding: 12px 12px 0 10px;
-  &.cat-description {
-    font-style: italic;
-    padding-top: 0;
-  }
-  &.product-placeholder {
-    padding-bottom: 12px;
-  }
-`
-const TitleInput = styled(TextField)`
-  color: red;
-  margin: 9px 0 1px 10px;
-  width: 100%;
-`
-const ProductList = styled.div`
-  padding: 5px 0 0;
-  transition: background-color 0.2s ease;
-  background-color: ${props => props.isDraggingOver ? props.palette.light : 'inherit'};
-  flex-grow: 1;
-`
-const AddButtonContainer = styled(Grid)`
-  padding: 10px 0 5px;
-`
 
 class MenuCategory extends Component {
   static contextType = ThemeContext
@@ -86,8 +45,13 @@ class MenuCategory extends Component {
 
   inputSubmit = () => {
     window.removeEventListener('mousedown', this.handleClick)
-    this.props.editCategory(this.state.category, this.props.index)
-    this.setState({ showCategoryInput: false })
+    this.props.editCategory(this.state.category)
+    this.setState({ showCategoryInput: false }, this.checkNameError())
+  }
+
+  checkNameError = () => {
+    this.props.category.categoryName !== this.state.category.categoryName
+      && this.setState({ category: { ...this.state.category, categoryName: this.props.category.categoryName } })
   }
 
   render() {
@@ -105,17 +69,21 @@ class MenuCategory extends Component {
               wrap="nowrap"
             >
               {this.state.showCategoryInput
-                ? <form onSubmit={this.inputSubmit} style={{ width: '99%' }} autoComplete="off">
+                ? <form
+                  onSubmit={this.inputSubmit}
+                  style={{ width: '99%' }}
+                  autoComplete="off"
+                >
                   <Grid container justify="space-between" alignItems="flex-end">
                     <Grid item>
                       <TitleInput
                         palette={palette}
-                        name="name"
+                        name="categoryName"
                         size="small"
                         label="Categoría"
                         type="text"
                         autoFocus
-                        value={this.state.category.name}
+                        value={this.state.category.categoryName}
                         onChange={this.handleInputChange}
                       />
                     </Grid>
@@ -130,16 +98,16 @@ class MenuCategory extends Component {
                   <TitleInput
                     style={{ width: '97%', marginBottom: '15px' }}
                     palette={palette}
-                    name="description"
+                    name="categoryDescription"
                     size="small"
                     label="Descripción"
                     type="text"
-                    value={this.state.category.description}
+                    value={this.state.category.categoryDescription}
                     onChange={this.handleInputChange}
                   />
                 </form>
                 : <Title palette={palette} variant="h5" noWrap>
-                  {capitalizeTheFirstLetterOfEachWord(this.props.category.name)}
+                  {capitalizeTheFirstLetterOfEachWord(this.props.category.categoryName)}
                 </Title>
               }
               <Grid item>
@@ -152,7 +120,7 @@ class MenuCategory extends Component {
                     ></Button>
                     <Button
                       style={{ minWidth: '0', padding: '5px 12px 5px 0' }}
-                      onClick={() => this.props.showConfirmationMessage(this.props.index, this.props.category._id)}
+                      onClick={() => this.props.showConfirmationMessage(this.props.index, this.props.category.categoryName)}
                       color="primary"
                       endIcon={<DeleteForeverIcon />}
                     ></Button>
@@ -160,9 +128,9 @@ class MenuCategory extends Component {
                 }
               </Grid>
             </TitleGrid>
-            {(this.props.category.description && !this.state.showCategoryInput) &&
+            {(this.props.category.categoryDescription && !this.state.showCategoryInput) &&
               <Title palette={palette} variant="subtitle1" noWrap className="cat-description">
-                {this.props.category.description.slice(0, 1).toUpperCase() + this.props.category.description.slice(1)}
+                {this.props.category.categoryDescription.slice(0, 1).toUpperCase() + this.props.category.categoryDescription.slice(1)}
               </Title>
             }
             <Divider style={{ margin: '12px -10px 0', zIndex: '999' }} />
@@ -174,28 +142,17 @@ class MenuCategory extends Component {
                   {...provided.droppableProps}
                   isDraggingOver={snapshot.isDraggingOver}
                 >
-                  {this.props.products
-                    .sort((a, b) => {
-                      return a.categories.find(cat => cat.id === this.props.category._id).index
-                        - b.categories.find(cat => cat.id === this.props.category._id).index
-                    })
-                    .map(product => (
-                      <Product
-                        key={product._id}
-                        category={this.props.category._id}
-                        product={product}
-                        index={product.categories.find(cat => cat.id === this.props.category._id).index}
-                        showConfirmationMessage={this.props.showConfirmationMessage}
-                        openProductForm={this.props.openProductForm}
-                        showProductTooltip={this.props.showProductTooltip}
-                        hideProductTooltip={this.props.hideProductTooltip}
+                  {this.props.category.products
+                    .map((elm, index) => (
+                      <MenuProduct
+                        key={elm.name}
+                        product={elm}
+                        index={index + 1}
+                        categoryIndex={this.props.index}
+                        removeProduct={this.props.removeProduct}
                       />
                     ))}
-                  {!this.props.products.some(prod => {
-                    return prod.categories.some(cat => {
-                      return cat.id === this.props.category._id
-                    })
-                  })
+                  {!this.props.category.products.length && !snapshot.isDraggingOver
                     && <Title variant="subtitle2" margin="normal" color="primary" className="product-placeholder">
                       Aún no has añadido productos a esta categoría.
                           </Title>
@@ -204,19 +161,6 @@ class MenuCategory extends Component {
                 </ProductList>
               )}
             </Droppable>
-
-            <Divider style={{ margin: '0 -10px' }} />
-
-            <AddButtonContainer container justify="flex-end">
-              <CustomButton
-                color="primary"
-                onClick={() => this.props.openProductForm(null, this.props.category._id)}
-                size="small"
-                startIcon={<AddBoxIcon />}
-              >
-                agregar producto
-              </CustomButton>
-            </AddButtonContainer>
           </CategoryContainer>
         )}
       </Draggable>
