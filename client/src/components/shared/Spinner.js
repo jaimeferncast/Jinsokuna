39dbd1cc73b5cf324e1cc3b47d68f05b7d9c8114
Jaime <jaimeferncast@gmail.com>
2021-06-07 20:16:57 +0200

    make spinner's margin prop dependent

--- client/src/components/shared/Spinner.js
+++ client/src/components/shared/Spinner.js
@@ -3,13 +3,13 @@ import styled from "styled-components"
 import { CircularProgress, Grid } from "@material-ui/core"
 
 const CustomCircularProgress = styled(CircularProgress)`
-  margin-top: 200px;
+  margin: ${props => props.margin ? props.margin : "200px 0 0"};
 `
 
 function Spinner(props) {
   return (
     <Grid container justify="center" alignItems="center">
-      <CustomCircularProgress size={80} thickness={2.5} />
+      <CustomCircularProgress size={80} thickness={2.5} margin={props.margin} />
     </Grid>
 
   )
