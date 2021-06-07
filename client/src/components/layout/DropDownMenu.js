ffc68606ebb52bab0d46b6c16c6c1a67b164d918
Jaime <jaimeferncast@gmail.com>
2021-06-07 09:15:22 +0200

    DropDownMenu: adjust new routes, add logout button and adjust styles

--- client/src/components/layout/DropDownMenu.js
+++ client/src/components/layout/DropDownMenu.js
@@ -1,4 +1,4 @@
-import { useState } from "react"
+import { useState, useContext } from "react"
 import { Link, withRouter } from "react-router-dom"
 
 import styled from "styled-components"
@@ -6,11 +6,19 @@ import styled from "styled-components"
 import { IconButton, Menu, MenuItem, Button } from "@material-ui/core"
 import MenuIcon from "@material-ui/icons/Menu"
 
+import ThemeContext from "../../ThemeContext"
+import CustomButton from "../shared/CustomButton"
+
 const DropDown = styled(Menu)`
-  border: '1px solid #333333';
+  & .MuiPopover-paper {
+    border: 1px solid #767676;
+    background-color: ${props => props.palette.dark};
+  }
 `
 
 function DropDownMenu(props) {
+  const { palette } = useContext(ThemeContext)
+
   const [anchorEl, setAnchorEl] = useState(null)
   const open = Boolean(anchorEl)
 
@@ -28,6 +36,7 @@ function DropDownMenu(props) {
         <MenuIcon />
       </IconButton>
       <DropDown
+        palette={palette}
         anchorEl={anchorEl}
         keepMounted
         open={open}
@@ -35,27 +44,38 @@ function DropDownMenu(props) {
       >
         <MenuItem>
           <Link to="/">
-            <Button
-              variant={props.location.pathname === "/" ? "outlined" : ""}
+            <CustomButton
+              width="-webkit-fill-available"
+              variant={props.location.pathname === "/" ? "outlined" : "text"}
               onClick={handleClose}
-            >Resumen de órdenes</Button>
+            >confección de carta</CustomButton>
           </Link>
         </MenuItem>
         <MenuItem>
-          <Link to="/carta">
-            <Button
-              variant={props.location.pathname === "/carta" ? "outlined" : ""}
+          <Link to="/pedidos">
+            <CustomButton
+              width="-webkit-fill-available"
+              variant={props.location.pathname === "/pedidos" ? "outlined" : "text"}
               onClick={handleClose}
-            >confección de carta</Button>
+            >Resumen de órdenes</CustomButton>
           </Link>
         </MenuItem>
         <MenuItem>
           <Link to="/usuario">
-            <Button
-              variant={props.location.pathname === "/usuario" ? "outlined" : ""}
+            <CustomButton
+              width="-webkit-fill-available"
+              variant={props.location.pathname === "/usuario" ? "outlined" : "text"}
               onClick={handleClose}
-            >Gestión de usuarios</Button>
+            >Gestión de usuarios</CustomButton>
           </Link>
+        </MenuItem>
+        <MenuItem>
+          <CustomButton
+            width="-webkit-fill-available"
+            variant="contained"
+            color="primary"
+            onClick={props.logoutUser}
+          >Cerrar sesión</CustomButton>
         </MenuItem>
       </DropDown>
     </div>
