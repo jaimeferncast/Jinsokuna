ae9130098d700ac422a694f5a23685635c2fc167
Jaime <jaimeferncast@gmail.com>
2021-06-14 15:23:44 +0200

    change button color

--- client/src/components/pages/Editor/EditMenu/CarteEditor/Category.js
+++ client/src/components/pages/Editor/EditMenu/CarteEditor/Category.js
@@ -161,13 +161,11 @@ class Category extends Component {
                     <Button
                       style={{ minWidth: '0', padding: '5px 12px 5px 0' }}
                       onClick={() => this.toggleInput()}
-                      color="primary"
                       endIcon={<EditIcon />}
                     ></Button>
                     <Button
                       style={{ minWidth: '0', padding: '5px 0 5px 0' }}
                       onClick={() => this.props.showConfirmationMessage(this.props.index, this.props.category._id)}
-                      color="primary"
                       endIcon={<DeleteForeverIcon />}
                     ></Button>
                   </Grid>
