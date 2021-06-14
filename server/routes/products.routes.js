997b02f59ca10d32afb49657f096169228e07618
Jaime <jaimeferncast@gmail.com>
2021-06-14 15:22:57 +0200

    avoid only menu products cleanup when fetching all products

--- server/routes/products.routes.js
+++ server/routes/products.routes.js
@@ -17,7 +17,7 @@ router.get('/', async (_req, res) => {
     const productsWithoutCategory = products.filter(prod => {
       return !prod.categories.some(cat => {
         return categoryIds.includes(cat.id.toString())
-      }) && !prod.isMenu
+      }) && !prod.isMenu && !prod.isMenuProduct
     })
 
     const deletedProducts = await Promise.all(productsWithoutCategory.map(prod => {
