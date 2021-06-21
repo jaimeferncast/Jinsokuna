f780aa391ddb1a53394f711a5b104c171fd9f38e
Jaime <jaimeferncast@gmail.com>
2021-06-21 15:09:24 +0200

    create oreder app with session service to fetch session upon loading

--- client/src/components/pages/OrderApp/OrderApp.js
+++ client/src/components/pages/OrderApp/OrderApp.js
@@ -1,10 +1,67 @@
+import { Component } from "react"
 
-function OrderApp(props) {
-  return (
-    <div>
-      Orders
-    </div>
-  )
+import ThemeContext from "../../../ThemeContext"
+import Spinner from "../../shared/Spinner"
+
+import SessionService from "../../../service/session.service"
+
+import { isMobileDevice } from "../../../utils"
+
+class OrderApp extends Component {
+  static contextType = ThemeContext
+
+  constructor(props) {
+    super()
+
+    this.state = {
+      headers: undefined,
+      alert: {
+        open: false,
+        message: "",
+        severity: undefined,
+        vertical: "bottom",
+      },
+    }
+    this.sessionService = new SessionService()
+  }
+
+  componentDidMount = async () => {
+    try {
+      const headers = (await this.sessionService.getSession()).data
+      this.setState({ headers, isMobileDevice: isMobileDevice(navigator.userAgent) })
+    }
+    catch (error) {
+      this.setState({
+        alert: {
+          open: true,
+          severity: "error",
+          message: "Error de servidor",
+          vertical: "bottom",
+        }
+      })
+    }
+  }
+
+  render() {
+    const { headers } = this.state
+    const { palette, font } = this.context
+
+    return (
+      <>
+        {headers
+          ? <div>
+            < p >
+              {headers?.referer}
+            </p >
+            <p>
+              {headers?.cookie}
+            </p>
+          </div >
+          : <Spinner />
+        }
+      </>
+    )
+  }
 }
 
 export default OrderApp
