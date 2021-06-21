f68bf218ff22dd26ff1a008d8d39e56c7ea45d87
Jaime <jaimeferncast@gmail.com>
2021-06-21 15:09:51 +0200

    work on menu component

--- client/src/components/pages/Menu/Menu.js
+++ client/src/components/pages/Menu/Menu.js
@@ -1,8 +1,22 @@
+import { useEffect } from "react"
+
+import { isMobileDevice } from "../../../utils"
 
 function Menu(props) {
+
+
   return (
     <div>
-      Menu para pedir desde mesa {props.location.search.slice(-2)}
+      {/* Menu para pedir desde mesa {props.location.search.slice(-2)} */}
+      <button onClick={() => isMobileDevice(navigator.userAgent) ? alert("eres móvil") : alert("no eres móvil")}>
+        user agent
+      </button>
+      <button onClick={() => console.log(document.headers)}>
+        referer
+      </button>
+      <button onClick={() => alert(new Date().toLocaleString().substr(0, 15))}>
+        fecha
+      </button>
     </div>
   )
 }
