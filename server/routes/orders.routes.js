20735ebdb8b9410e662596eb7bb56af791f617f6
Jaime <jaimeferncast@gmail.com>
2021-06-27 22:38:54 +0200

    add cookie handling in get order by table

--- server/routes/orders.routes.js
+++ server/routes/orders.routes.js
@@ -2,6 +2,29 @@ const express = require('express')
 const router = express.Router()
 const Order = require('../models/order.model')
 
+router.get("/:table", async (req, res) => {
+  try {
+    if (!req.cookies.orderId) {
+      const order = await Order.findOne({ table: req.params.table, status: "active" })
+      if (order) {
+        res.cookie("orderId", order._id)
+        res.status(200).json(order)
+      }
+      else {
+        const order = await Order.create({ table: req.params.table })
+        res.cookie("orderId", order._id)
+        res.status(200).json(order)
+      }
+    } else {
+      const order = await Order.findOne({ _id: req.cookies.orderId })
+      if (order.status === "active") res.status(200).json(order)
+      else {
 
+      }
+    }
+  } catch (error) {
+    res.status(500).json({ ...error, message: error.message })
+  }
+})
 
 module.exports = router
