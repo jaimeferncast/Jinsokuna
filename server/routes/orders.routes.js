fa6dfdd5013be84f3eb640ad1ad362c21172aa3e
Jaime <jaimeferncast@gmail.com>
2021-06-28 10:29:51 +0200

    finish order get route

--- server/routes/orders.routes.js
+++ server/routes/orders.routes.js
@@ -3,24 +3,28 @@ const router = express.Router()
 const Order = require('../models/order.model')
 
 router.get("/:table", async (req, res) => {
+  console.log(req.cookies)
   try {
-    if (!req.cookies.orderId) {
+    if (!req.cookies.orderId) { // client has no cookies
       const order = await Order.findOne({ table: req.params.table, status: "active" })
       if (order) {
-        res.cookie("orderId", order._id)
+        res.cookie("orderId", order._id, { maxAge: 86400000, path: "/" })
         res.status(200).json(order)
       }
       else {
         const order = await Order.create({ table: req.params.table })
-        res.cookie("orderId", order._id)
+        res.cookie("orderId", order._id, { maxAge: 86400000, path: "/" })
         res.status(200).json(order)
       }
-    } else {
+    } else { // client has cookies
       const order = await Order.findOne({ _id: req.cookies.orderId })
-      if (order.status === "active") res.status(200).json(order)
-      else {
-
+      if (!order || order?.status === "closed") {
+        res.clearCookie("orderId", { path: "/" })
+        const order = await Order.create({ table: req.params.table })
+        res.cookie("orderId", order._id, { maxAge: 86400000, path: "/" })
+        res.status(200).json(order)
       }
+      else res.status(200).json(order)
     }
   } catch (error) {
     res.status(500).json({ ...error, message: error.message })
